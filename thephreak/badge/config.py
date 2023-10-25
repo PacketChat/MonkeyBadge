e319e47f3ea30d02e35587cae1c9e83b671b81b8
ken williams <kwillia@gmail.com>
2023-10-25 11:06:03 -0700

    broke up program into modules, created async execution cycle for main loop

--- thephreak/badge/config.py
+++ thephreak/badge/config.py
@@ -1,6 +1,18 @@
 # configuration settings
 
-SSID = "WillHouse6"
-SSID_PASSWORD = "williams123"
+# Wifi Config
+WIFI_SSID = "WillHouse6"
+WIFI_PASSWORD = "williams123"
+
+# badge config
 REG_KEY = "7bc78281-2036-41b2-8d98-fc23ec504e9a"
 API_SERVER = "http://192.168.2.165:8000"
+BUTTON_PINS = [4, 14, 15, 13]
+
+# OLED screen Config
+OLED_HEIGHT = 64    # height (int): Height of the display in pixels.
+OLED_WIDTH = 128    # width (int): Width of the display in pixels.
+OLED_SDA_PIN = 5    # sda_pin (int): Pin number for the SDA (data) line of the I2C bus.
+OLED_SCL_PIN = 23   #scl_pin (int): Pin number for the SCL (clock) line of the I2C bus.
+
+
--- thephreak/badge/main.py
+++ thephreak/badge/main.py
@@ -1,145 +1,44 @@
-import network
-import config
+import uasyncio as asyncio
 import urequests as requests
-import urandom
-import btree
-import json
+import ujson as json
+import re 
 
-from time import sleep
+import config  # Import the config file
 
-def uuid4():
-    # Generate 32 random bits at a time, 4 times, and concatenate them, because
-    # our MicroPython supports only 32 bits of random bits:
-    uuid_bytes = bytes()
-    for _ in range(4):
-        uuid_bytes += urandom.getrandbits(32).to_bytes(4, 'big')
+from lib import wifi
+from lib import menu
+from lib.button import ButtonHandler
+from lib import gameclient
 
-    # Format the bytes as a UUID:
-    uuid_str = '-'.join(
-        [uuid_bytes[:4].hex(), uuid_bytes[4:6].hex(), uuid_bytes[6:8].hex(),
-        uuid_bytes[8:10].hex(), uuid_bytes[10:].hex()])
+def display_mainmenu(display):
+    display.clear()
+    display.show_text("Main Menu","> Status", "> Radio", "> MatchMode", "> About")
 
-    return uuid_str
+async def main():
+    display = menu.DisplayHandler(config.OLED_WIDTH, config.OLED_HEIGHT, config.OLED_SDA_PIN, config.OLED_SCL_PIN)
+    display.clear()
+    display.show_text("Badge Booting", "Please Wait...")
 
+    # Setup WiFi
+    wifi_manager = wifi.WiFiManager(config.WIFI_SSID, config.WIFI_PASSWORD)
+    await wifi_manager.connect()
 
-class NetworkMGR:
-    def __init__(self):
-        self.wlan = network.WLAN(network.STA_IF)
-        self.wlan.active(True)
+    # startup the badge, use the mac as the UUID
+    mymac = re.sub(':', '-', str(wifi_manager.get_mac()))
+    badge = gameclient.MonkeyBadge(mymac)
 
-    def connect_wifi(self, SSID, SSID_PASSWORD):
-        self.wlan.active(True)
-        if not self.wlan.isconnected():
-            self.wlan.connect(SSID, SSID_PASSWORD)
-            while not self.wlan.isconnected():
-                pass
-    def get_status(self):
-        return self.wlan.ifconfig()
-    
-    def isWifiConnected(self):
-        if not self.wlan.isconnected():
-            return False
-        else:
-            return True
+    display_mainmenu(display)
 
-class MonkeyBadge:
-    def __init__(self, uuid):
-        self.badge_uuid = uuid
-        self.registration_key = config.REG_KEY
-        self.handle = ""
-        self.apitoken
+    # Initialize the button handler with the pins from the config file
+    button_handler = ButtonHandler(config.BUTTON_PINS)
 
-    def register(self):
-        baseurl = config.API_SERVER
+    # Create tasks
+    button_check_task = asyncio.create_task(button_handler.check_buttons())
+    checkin_task = asyncio.create_task(badge.checkin())
 
-        # get a handle from the server
-        r = requests.get(baseurl + "/generate_handle")
-        if r.status_code == 200:
-            self.handle = r.text
-        else:
-            self.handle = "unnamed_monkey"
-    
-        print("Running Badge Registration")
-        print("UUID: %s, handle: %s" %(self.badge_uuid, self.handle))
-        print("connecting to %s" % (baseurl + "/register"))
-        print("JSON Values: %s" %({"handle": self.handle, "key": self.registration_key, "myUUID": self.badge_uuid}))        
-
-        r = requests.post(baseurl + "/register", json={"myUUID": self.badge_uuid, "handle": self.handle, "key": self.registration_key })
-        json_data = json.loads(r.text)
-        print(json_data)
-        if 'UUID' in json_data and r.status_code == 200:
-            print("badge registered")
-            self.apitoken = json_data['token']
-        else:
-            print("badge not registered.")
-
-    def checkin(self):
-        print("[Method]: checkin")
-        baseurl = config.API_SERVER
-
-        print("connecting to %s" % (baseurl + "/checkin"))
-
-        print("token: %s, uuid: %s" %(self.apitoken, self.badge_uuid))
-        r = requests.post(baseurl + "/checkin", headers={'X-API-Key': self.apitoken }, json={"myUUID": self.badge_uuid})
-        json_data = json.loads(r.text)
-        print("server response: %s" % (json_data))
-        if r.status_code == 200:
-            print("badge registered")
-            return True
-        else:
-            print("badge not registered.")    
-            return False    
-
-def main():
-
-    # db initialize
-    try:
-        file = open("badgedb", "r+b")
-    except OSError:
-        file = open("badgedb", "w+b")
-    
-    db = btree.open(file)
-
-    badge = ""
-    # do I have a valid uuid already generated?
-    if not b"UUID" in db:
-        db[b'UUID'] = uuid4()
-        db.flush()
-        print("Setting my UUID to %s" %(db[b'UUID']))
-        badge = MonkeyBadge(db[b'UUID'].decode)
-    else:
-        print("Getting my UUID to %s" %(db[b'UUID']))
-        badge = MonkeyBadge(db[b'UUID'].decode)
-
-    net = NetworkMGR()
-
-
-    print("Starting main program loop")
+    # Run tasks concurrently
     while True:
-        #sleep(60)
-        # check if wifi is connected
-        print("Checking Wifi Connectivity")
-        if not net.isWifiConnected():
-            print("not connected, trying to connect.")
-            net.connect_wifi(config.SSID, config.SSID_PASSWORD)
-            #sleep(10)
-        else:
-            print("Network Connected:")
-            print(net.get_status())
-            print("checking if badge is registered")
-
-            if not b"token" in db:
-                # we need to register
-                print("badge not registered, contacting registration server")
-                badge.register()
-            else:
-
-                badge.apitoken = db[b'token'].decode()
-                
-            if badge.checkin() == True:
-                print("badge check-in success.")
-
-
-if __name__ == "__main__":
-    main()
-
+        await asyncio.gather(button_check_task, checkin_task)
+    
+# Run the main function
+asyncio.run(main())
